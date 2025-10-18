69f1e4dd709271bf231096ca54dcf895c0298764
markbozsoki <bozsoki.mark@icloud.com>
2025-10-18 21:54:17 +0200

    fix

--- app/Models/Kar.php
+++ app/Models/Kar.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Masolas extends Model
+class Kar extends Model
 {
     use HasFactory;
 
--- database/seeders/HallgatoSeeder.php
+++ database/seeders/HallgatoSeeder.php
@@ -2,25 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hallgato;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Hallgato extends Seeder
+class HallgatoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Hallgato::truncate();
+        //Hallgato::truncate();
         $csvFile = fopen(base_path('database/csv_sources/hallgato.csv'), 'r');
         $isFirstLine = true;
-        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
+        while (($data = fgetcsv($csvFile, 2000, '	')) !== false) {
             if (! $isFirstLine) {
                 Hallgato::create([
-                    'nev' => $data[1],
-                    'osztondijas' => $data[2],
-                    'kar_id' => $data[3],
+                    'nev' => $data['1'],
+                    'osztondijas' => $data['2'],
+                    'kar_id' => $data['3'],
                 ]);
             }
             $isFirstLine = false;

f0d9f9d204492a8ed542c7d5f57b032f545e9534
MoSzabina <moszabina@gmail.com>
2025-10-27 15:27:45 +0100

    admin_page_and_route

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/admin', function () {
+    if (auth()->check() && auth()->user()->role === 'admin') {
+        return view('admin');
+    }
+    abort(403);
+})->name('admin');

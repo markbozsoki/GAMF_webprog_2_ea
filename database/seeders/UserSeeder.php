a630444da4a2b62de2df37193ac81b46e712a3ef
MoSzabina <moszabina@gmail.com>
2025-10-27 15:10:47 +0100

    created_seeder_for_userstable

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
+        ]);
+
+        // Test users
+        User::factory(5)->create();
+    }
+}
